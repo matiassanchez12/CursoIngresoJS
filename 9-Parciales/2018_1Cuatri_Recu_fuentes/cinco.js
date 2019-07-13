b5baf5c3571fd7442b0d656d462ec05732dfc810
matiassanchez12 <52495199+matiassanchez12@users.noreply.github.com>
2019-07-13 12:27:40 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,9 @@
 function mostrar()
 {
+    var num1, num2;
+
+     prompt("ingrese un numero : ");
+
+    num2 = prompt ("ingrese otro numero : ");
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+    var num1, num2, suma, dividir;
 
+    num1 = parseInt (prompt ("ingrese un numero : "));
+    num2 = parseInt (prompt ("ingrese otro numero: "));
+    
+    if (num1 == num2){
+        alert ("los numeros : " + num1 + " y " + num2 + " son iguales.");
+    }
+    else{
+        if (num1 > num2){ //son distintos
+        dividir = num1 / num2;
+        alert ("esto es una division : " + dividir);
+        }
+        else{ //son distintos
+        suma = num1 + num2;
+        }
+        if (suma < 50 ){ //resultado, si es menor a 50
+        alert ("la suma es : " + suma + " y es menor a 50.");
+    }
+    }
 }
