d6f83d8b8592daecbf2a421fe4ac889eba70f1a4
matiassanchez12 <52495199+matiassanchez12@users.noreply.github.com>
2019-08-02 23:29:46 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -50,7 +50,7 @@
 	document.writeln("1-Suma de los negativos:"+negativos,"\n2-Suma de los positivos:"+positivos,"\n3-Cantidad de positivos:"+cPositivos,"\n4-Cantidad de negativos:"+cNegativos,"\n5-Cantidad de ceros:    "+cCeros,"\n6-Cantidad de numeros pares:"+ cPares,"\n7-Promedio de positivos:"+pPositivos,"\n8-Promedio de negativos:"+pNegativos,"\n9-Diferencia de negativos y positivos:"+diferencia);
 
 }//FIN DE LA FUNCIÓN
-*/
+*//*
 function mostrar() {
 
 
@@ -126,4 +126,69 @@ function mostrar() {
 
 
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN*/
+function mostrar() {
+	var numero;
+	var seguir = "s";
+	var acumPositivos=0;
+	var acumNegativos=0;
+	var contPositivos=0;
+	var contNegativos=0;
+	var contCeros=0;
+	var contPares=0;
+	var promPositivos;
+	var promNegativos;
+	var diferenciaPosNeg;
+
+	while (seguir == "s"){
+
+	numero = parseInt(prompt("ingrese un numero: "));
+
+	if (numero > 0){
+
+		acumPositivos = acumPositivos + numero;
+		contPositivos = contPositivos + 1;
+	}
+	else if (numero < 0){
+
+		acumNegativos = acumNegativos + numero;
+		contNegativos = contNegativos + 1;
+	}
+	else {
+		contCeros = contCeros + 1;
+	}
+		
+	if ( numero % 2 == 0){
+		contPares = contPares + 1;
+	}
+
+	seguir = prompt ("seguir ?");
+}
+
+	if (contPositivos != 0)
+	{
+	promPositivos = acumPositivos / contPositivos;
+	}
+	else {
+		promPositivos = "No existe promedio para positivos."
+	}
+	if (contNegativos != 0)
+	{
+	promNegativos = acumNegativos / contNegativos;
+	}
+	else {
+	promNegativos = "No existe promedio para Negativos."
+	}
+
+	diferenciaPosNeg = acumPositivos - (-acumNegativos); 
+
+	document.write ("1-Suma de los negativos :" + acumNegativos + "<br>");
+	document.write("2-Suma de los positivos:"+ acumPositivos + "<br>"); 
+	document.write("3-Cantidad de positivos:"+ contPositivos + "<br>"); 
+	document.write("4-Cantidad de negativos:"+ contNegativos + "<br>");
+	document.write("5-Cantidad de ceros:"+ contCeros + "<br>");
+	document.write("6-Cantidad de números pares:"+ contPares + "<br>"); 
+	document.write("7-Promedio de positivos:"+ promPositivos + "<br>");
+	document.write("8-Promedios de negativos:"+ promNegativos + "<br>");
+	document.write("9-Diferencia entre positivos y negativos, (positvos-negativos):"+ diferenciaPosNeg + "<br>");
+}
