16e0d93ba6111fc3792ce56567d4a28b7ef1dea7
matiassanchez12 <52495199+matiassanchez12@users.noreply.github.com>
2019-08-31 12:13:37 -0300

    Add files via upload

---
+++ claseS/Misfunciones.c
@@ -0,0 +1,34 @@
+int sumar (int primerNumero, int segundoNumero)
+
+int calculadora (int primerNumero, int segundoNumero, int operacion)
+{
+    switch (operacion)
+    {
+
+        case 1: dividir(primerNumero, segundoNumero);
+        break;
+        case 2: restar(primerNumero, segundoNumero);
+        break;
+        case 3: multiplicar(primerNumero, segundoNumero);
+        break;
+        case 4: dividir(primerNumero, segundoNumero);
+        break;
+    }
+}
+
+int pedirNumero(){
+    int numero;
+    printf("ingrese numero");
+    scanf("%d", &numero);
+    return numero;
+
+}
+
+void MostrarResultado
+
+void hacerCalculo(){
+    int num1;
+    int num2;
+
+
+}
---
+++ claseS/misFunciones.h
@@ -0,0 +1,7 @@
+#ifndef MISFUNCIONES_H_INCLUDED
+#define MISFUNCIONES_H_INCLUDED
+
+int calcular (int primerNumero, int segundoNumero, int operacion);
+
+
+#endif // MISFUNCIONES_H_INCLUDED
